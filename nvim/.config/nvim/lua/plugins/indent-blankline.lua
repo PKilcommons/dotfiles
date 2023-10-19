18232eeb341ffe5a50f77ae283389f219659bff4
PKilcommons <pmkilcommons@gmail.com>
2023-10-19 21:32:20 +0000

    nvim: add package

---
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+--local highlight = {"CursorColumn", "Whitespace"}
+return {
+  -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help indent_blankline.txt`
+  main = "ibl",
+  opts = {
+    indent = {
+      char = ".",
+      smart_indent_cap = false,
+      --highlight = highlight,
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,55 @@
+return {
+  -- NOTE: First, some plugins that don't require any configuration
+
+  -- Git related plugins
+  'tpope/vim-fugitive',
+  'tpope/vim-rhubarb',
+
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- "gc" to comment visual regions/lines
+  {
+    'numToStr/Comment.nvim', opts = {}
+  },
+
+  -- Useful plugin to show you pending keybinds.
+  {
+    'folke/which-key.nvim', opts = {}
+  },
+
+  {
+    -- Adds git releated signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '│' },
+        change = { text = '│' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '|' },
+      },
+      on_attach = function(bufnr)
+        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk, { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
+        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
+        vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
+      end,
+    },
+  },
+
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'material',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+}
