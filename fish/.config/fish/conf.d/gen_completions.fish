aee1e842a53e4d1d951a0a01e07b317c99ef1e58
PKilcommons <pmkilcommons@gmail.com>
2025-05-17 05:49:11 +0000

    fish: add `uv` completions

--- fish/.config/fish/conf.d/gen_completions.fish
+++ fish/.config/fish/conf.d/gen_completions.fish
@@ -1,3 +1,7 @@
 if command -q jj
     jj util completion fish > "$HOME/.config/fish/completions/jj.fish"
 end
+
+if command -q uv
+    uv generate-shell-completion fish > "$HOME/.config/fish/completions/uv.fish"
+end
