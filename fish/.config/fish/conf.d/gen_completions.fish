29f9ccb09df7c0c31a1fde40c9032e873dcb8d84
PKilcommons <pmkilcommons@gmail.com>
2025-06-02 02:27:35 +0000

    fish: untrack autogen completions
    These are automatically generated on each shell call so there's little
    reason to tracking them in version control.

--- fish/.config/fish/conf.d/gen_completions.fish
+++ fish/.config/fish/conf.d/gen_completions.fish
@@ -1,7 +1,10 @@
+set -l COMP_PATH "$HOME/.config/fish/completions"
+mkdir -p $COMP_PATH
+
 if command -q jj
-    jj util completion fish > "$HOME/.config/fish/completions/jj.fish"
+    jj util completion fish > "$COMP_PATH/jj.fish"
 end
 
 if command -q uv
-    uv generate-shell-completion fish > "$HOME/.config/fish/completions/uv.fish"
+    uv generate-shell-completion fish > "$COMP_PATH/uv.fish"
 end
